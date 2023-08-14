2b4996fab9dffc15c41fd17f17601e8df8ac9c18
govindasai <govindasai.s@marketsimplified.com>
2023-08-14 11:23:49 +0530

    commiting sec project

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -9,7 +9,7 @@ fun main() {
     embeddedServer(Netty, port = 8082, host = "127.0.0.2", module = Application::module)
         .start(wait = true)
 }
-
+//hello send commit updation
 fun Application.module() {
     configureRouting()
 }
